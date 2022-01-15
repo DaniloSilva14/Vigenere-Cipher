2e1c5c5943e78b219a2303ddcc3a74792d6b04c2
Danilo Silva <danilo_rafhael@hotmail.com>
2022-01-14 23:43:27 -0300

    Implementando encriptacao e decriptacao

--- script.js
+++ script.js
@@ -13,21 +13,20 @@ function encode() {
   var input = document.getElementById('input').value;
   var key = document.getElementById('key').value;
 
-  console.log(generateKey(input, key)); 
-  
-
-  var output = input + key
-
-  //console.log('input', input);
-  //console.log('key', key);
-  //console.log('output', output);
+  var useKey = generateKey(input, key)
+  var output = cipherEncryptText(input.toUpperCase(), useKey)
 
   document.getElementById('output').value = output;
 }
 
 function decode() {
   var input = document.getElementById('input').value;
-  document.getElementById('output').value = input;
+  var key = document.getElementById('key').value;
+
+  var useKey = generateKey(input, key)
+  var output = cipherDecryptText(input.toUpperCase(), useKey)
+
+  document.getElementById('output').value = output;
 }
 
 // scripts vigenere
@@ -45,5 +44,38 @@ function generateKey(str, key) {
     }
   }
 
-  return key.join("");
+  return key.join("").toUpperCase();
+}
+
+// algoritmo encriptacao da cifra de Vigenere 
+function cipherEncryptText(str, key) {
+  let cipher_text = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if(str[i] == " ") {
+      cipher_text += " ";
+    } else {
+      let x = (str[i].charCodeAt(0) + key[i].charCodeAt(0)) % 26;
+      x += 'A'.charCodeAt(0);
+      cipher_text += String.fromCharCode(x);
+    }
+  }
+
+  return cipher_text;
+}
+
+// algoritmo decriptacao da cifra de Vigenere 
+function cipherDecryptText(str, key) {
+  let orig_text = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if(str[i] == " ") {
+      orig_text += " ";
+    } else {
+      let x = (str[i].charCodeAt(0) - key[i].charCodeAt(0) + 26) % 26;
+      x += 'A'.charCodeAt(0);
+      orig_text += String.fromCharCode(x);
+    }
+  }
+  return orig_text;
 }
