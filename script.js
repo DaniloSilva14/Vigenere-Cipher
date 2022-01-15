484a2543735d799826e4e1e96ba757d731d11463
Danilo Silva <danilo_rafhael@hotmail.com>
2022-01-14 22:46:48 -0300

    Implementado algoritmo do tamanho da chave

--- script.js
+++ script.js
@@ -6,22 +6,44 @@ function chooseSelectTo() {
 function calculate() {
   var select = document.getElementById('selectFrom').value;
 
-  select == 'encode' ? encode() :  decode();
+  select == 'encode' ? encode() : decode();
 }
 
 function encode() {
   var input = document.getElementById('input').value;
   var key = document.getElementById('key').value;
 
-  console.log('input', input);
-  console.log('key', key);
+  console.log(generateKey(input, key)); 
+  
 
   var output = input + key
-  console.log('output', output);
+
+  //console.log('input', input);
+  //console.log('key', key);
+  //console.log('output', output);
+
   document.getElementById('output').value = output;
 }
 
 function decode() {
   var input = document.getElementById('input').value;
   document.getElementById('output').value = input;
+}
+
+// scripts vigenere
+
+// essa funcao gera uma chave do tamanho do input
+function generateKey(str, key) {
+  key = key.split("");
+
+  if (str.length == key.length)
+    return key.join("");
+  else {
+    let temp = key.length;
+    for (let i = 0; i < (str.length - temp); i++) {
+      key.push(key[i % ((key).length)])
+    }
+  }
+
+  return key.join("");
 }
